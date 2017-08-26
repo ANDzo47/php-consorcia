9424a176331b5f7917e05f528326e2ba5f130e29
Andres Rizzo <andyrizzo@gmail.com>
2017-08-26 10:02:24 -0300

    Initial commit

---
+++ application/controllers/buildings.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Buildings extends MY_Controller {
+
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+        
+	public function index()
+	{
+            $data['buildings'] = Building::find('all');            
+            $this->load->view('common/header');
+            $this->load->view('building/building_home',$data);            
+            $this->load->view('common/footer');
+	}
+     
+}
+
+/* End of file building.php */
+/* Location: ./application/controllers/building.php */
---
+++ application/models/ExtraordinaryTransaction.php
@@ -0,0 +1,23 @@
+<?php
+
+class ExtraordinaryTransaction extends ActiveRecord\Model{
+    
+    static $belongs_to = array(
+        array('property'),
+        array('extraordinary_period')
+    );
+    
+    public static function exist_bank_payment($bank_payment_id) {
+		$income = ExtraordinaryTransaction::find_by_sql("SELECT et.*
+                                                   		   FROM extraordinary_transactions et 
+                                                  		  WHERE et.bank_payment_id = '$bank_payment_id'");
+
+        if (count($income) > 0) {
+            return true;
+        } else{
+            return false;
+        }
+	}
+
+}
+?>
