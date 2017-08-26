6e893c57b453a243f170f2e366c1b41ae95cc1cd
Andres Rizzo <andyrizzo@gmail.com>
2017-08-26 19:28:16 -0300

    me hinche los huevos

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 
-$config['base_url']	= '';
+$config['base_url']	= 'http://php-consorcia-consortium.a3c1.starter-us-west-1.openshiftapps.com';
 
 /*
 |--------------------------------------------------------------------------
